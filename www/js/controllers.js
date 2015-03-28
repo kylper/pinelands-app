0ecff3bf5c38c6c9cd642e854d7336a8e3d6a6c8
Kyle Permuy <kyle@varninja.com>
2015-03-28 16:38:23 -0400

    Last Tab View update

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -20,17 +20,6 @@ angular.module('starter.controllers', [])
   };
 })
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
-})
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
 .controller('ArtistsCtrl', function($scope, Artists) {
   $scope.artists = Artists.all();
 })
